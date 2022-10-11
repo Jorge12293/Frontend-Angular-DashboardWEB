f9adf6f655e8c5996d5e887f7d3c1a0700e88e64
Jorge Rivera <jorgedesarrollo717@gmail.com>
2022-10-10 23:09:38 -0500

    Modificacion de formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  
+ 
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  public registerForm = this.fb.group({
+    nombre:['Jorge',[Validators.required,Validators.minLength(3)]],
+    email:['jorge@gmail.com',[Validators.required]],
+    password:['123456789',[Validators.required]],
+    password2:['123456789',[Validators.required]],
+    terminos:[true,[Validators.required]],
+  });
+
+  crearUsuario(){
+    console.log(this.registerForm.value)
+  }
 }
